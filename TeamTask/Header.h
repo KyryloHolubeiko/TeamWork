62c1c6cee6e7ae077bfddaf61011e2dbf2efd989
KyryloHolubeiko <kirillgolubejko@gmail.com>
2020-08-27 20:14:47 +0300

    Commit_Dog

--- TeamTask/Header.h
+++ TeamTask/Header.h
@@ -2,3 +2,25 @@
 #include <string>
 
 using namespace std;
+
+class Dog {
+	string name;
+	string type;
+public:
+	Dog(string name, string type) { this->name = name; this->type = type; }
+
+	void Sound() {
+		cout << "GAV GAV" << endl;
+	}
+	void Show() {
+		cout << "Name" << name << endl;
+	}
+
+	void Type() {
+		cout << "Type" << type << endl;
+	}
+
+	void Info() {
+		cout << "Name: " << name << endl << "Age: 10" << endl << "Sex: male" << endl << "10kg" << endl;
+	}
+};
