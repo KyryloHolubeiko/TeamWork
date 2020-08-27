b1e6c07ef95410a9a683f171a7226f9632830081
Bogdan-Shklanka2002 <bogdan.shklanka@gmail.com>
2020-08-27 20:37:14 +0300

    classHamster

--- TeamTask/Header.h
+++ TeamTask/Header.h
@@ -7,20 +7,35 @@ class Hamster
 {
 	string name;
 	int age;
-	string age;
+	string sex;
 	int weight;
 public:
 	Hamster();
 	~Hamster();
-
-private:
+	void Sound()const {
+		cout << "PI PI PI PI \n";
+	}
+	void ShowName()const {
+		cout << " Name: " << name << endl;
+	}
+	void GetType()const {
+		cout << "Type: " << "rodent \n";
+	}
+	void ShowInfo()const {
+		ShowName();
+		cout << "Age: " << age << endl;
+		cout << "Sex: " << sex << endl;
+		cout << "Weight: " << weight << endl;
+	}
 
 };
 
 Hamster::Hamster()
 {
-}
+	name = "Hamchi";
+	age = 0;
+	sex = "male";
+	weight = 0;
 
-Hamster::~Hamster()
-{
 }
+Hamster::~Hamster(){}
